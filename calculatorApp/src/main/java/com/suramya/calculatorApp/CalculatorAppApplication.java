efa8b220ba582874f1179130a3adc3e756015c32
suramya-biswas-git <suramya.biswas@gmail.com>
2024-06-24 18:27:27 +0530

    Second Version

--- calculatorApp/src/main/java/com/suramya/calculatorApp/CalculatorAppApplication.java
+++ calculatorApp/src/main/java/com/suramya/calculatorApp/CalculatorAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(CalculatorAppApplication.class, args);
 	}
 
---
+++ calculatorApp/src/main/java/com/suramya/calculatorApp/service/MyService.java
@@ -0,0 +1,8 @@
+package com.suramya.calculatorApp.service;
+
+public class MyService {
+	public void show() {
+		System.out.println("hi");
+	}
+
+}
